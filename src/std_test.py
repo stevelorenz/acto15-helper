cae30a6b75798ef082f7c89fb4ea42d1f679ce6e
Zuo Xiang <xianglinks@gmail.com>
2016-01-05 13:07:46 +0100

    finishe the first step of data processing

---
+++ src/std_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# #####################################
+# Author : Xiang,Zuo
+# E-Mail : xianglinks@gmail.com
+# Date   :
+# About  :
+# #####################################
+
+
+import numpy
+
+a = [1, 2, 3, 4]
+b = numpy.average(a)
+c = numpy.std(a)
+
+print(b)
+print(c)
